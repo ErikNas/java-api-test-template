ef707b45bcd5df34a04c562d9bd1a7eb6f1d2e6f
ErikNas <maileriknas@gmail.com>
2025-04-15 09:36:53 +0300

    Изменил структуру. Добавил пример реализации теста для PetStore

---
+++ examples/petstore-example/src/test/java/org/example/tests/PetStoreTestExample.java
@@ -0,0 +1,44 @@
+package org.example.tests;
+
+import io.restassured.http.ContentType;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PetStoreTestExample {
+
+    @Test
+    void createNewPetTest() {
+        String uri = "https://petstore.swagger.io/v2/pet/";
+        String petName = "Барсик";
+
+        JSONObject bodyJO = new JSONObject()
+                .put("name", petName)
+                .put("status", "available");
+
+        String newPetId = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(bodyJO.toString())
+                .post(uri)
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getString("id");
+
+        String actualPetName = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(uri + newPetId)
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getString("name");
+
+        Assertions.assertEquals(petName, actualPetName);
+    }
+}
